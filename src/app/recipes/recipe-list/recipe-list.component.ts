81b87ea836e3b15303e75c135b9c71dcb49645ca
Arm Suwansiri <ysuwansiri@yahoo.com>
2016-11-21 14:06:19 +0700

    finished 96

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,9 +9,7 @@ import { RecipeService } from '../recipe.service';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [];
-
-  @Output() recipeSelected = new EventEmitter();
-
+  
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
@@ -19,7 +17,7 @@ export class RecipeListComponent implements OnInit {
   }
 
   onSelected(recipe: Recipe) {
-    this.recipeSelected.emit(recipe);
+    
   }
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -12,6 +12,7 @@ export class RecipesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
